709b9cd88e01cbe74067ab62b6f2934235180b98
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-02-18 18:34:23 +0100

    feat: Added basic navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-navbar',
-    imports: [],
+    imports: [RouterLink, RouterLinkActive, NgbCollapse],
     templateUrl: './navbar.html',
     styleUrl: './navbar.scss',
 })
 export class Navbar {
+    isMenuCollapsed = signal(true);
 
+    toggleMenu() {
+        this.isMenuCollapsed.update(value => !value);
+    }
+
+    closeMenu() {
+        this.isMenuCollapsed.set(true);
+    }
 }
